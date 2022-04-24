915f053b192f1d5f81d415a9d28e42238f4de296
Dan199312 <dan199312@gmail.com>
2022-04-24 15:24:48 +0200

    last part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,11 @@ import { TimerComponent } from './timer/timer.component';
 import { UsersComponent } from './users/users.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HighlightDirective } from './highlight.directive';
+import { ReversePipe } from './reverse.pipe';
+import { SortPipe } from './sort.pipe';
 
 @NgModule({
-  declarations: [AppComponent, CounterComponent, TodosComponent, TodoComponent, TimerComponent, UsersComponent, HighlightDirective],
+  declarations: [AppComponent, CounterComponent, TodosComponent, TodoComponent, TimerComponent, UsersComponent, HighlightDirective, ReversePipe, SortPipe],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule,],
   providers: [],
   bootstrap: [AppComponent],
